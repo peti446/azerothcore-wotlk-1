0d767ec4afc39514ddd1a9a369683107d812fbaa
peti446 <peti446@users.noreply.github.com>
2020-09-20 23:00:18 +0100

    fix: DB to use new standard

--- data/sql/updates/pending_db_world/rev_1591834081871049400.sql
+++ data/sql/updates/pending_db_world/rev_1591834081871049400.sql
@@ -30,22 +30,3 @@ DELETE FROM `item_template` WHERE `entry`=36763;
 INSERT INTO `item_template` (`entry`, `class`, `subclass`, `SoundOverrideSubclass`, `name`, `displayid`, `Quality`, `Flags`, `FlagsExtra`, `BuyCount`, `BuyPrice`, `SellPrice`, `InventoryType`, `AllowableClass`, `AllowableRace`, `ItemLevel`, `RequiredLevel`, `RequiredSkill`, `RequiredSkillRank`, `requiredspell`, `requiredhonorrank`, `RequiredCityRank`, `RequiredReputationFaction`, `RequiredReputationRank`, `maxcount`, `stackable`, `ContainerSlots`, `StatsCount`, `stat_type1`, `stat_value1`, `stat_type2`, `stat_value2`, `stat_type3`, `stat_value3`, `stat_type4`, `stat_value4`, `stat_type5`, `stat_value5`, `stat_type6`, `stat_value6`, `stat_type7`, `stat_value7`, `stat_type8`, `stat_value8`, `stat_type9`, `stat_value9`, `stat_type10`, `stat_value10`, `ScalingStatDistribution`, `ScalingStatValue`, `dmg_min1`, `dmg_max1`, `dmg_type1`, `dmg_min2`, `dmg_max2`, `dmg_type2`, `armor`, `holy_res`, `fire_res`, `nature_res`, `frost_res`, `shadow_res`, `arcane_res`, `delay`, `ammo_type`, `RangedModRange`, `spellid_1`, `spelltrigger_1`, `spellcharges_1`, `spellppmRate_1`, `spellcooldown_1`, `spellcategory_1`, `spellcategorycooldown_1`, `spellid_2`, `spelltrigger_2`, `spellcharges_2`, `spellppmRate_2`, `spellcooldown_2`, `spellcategory_2`, `spellcategorycooldown_2`, `spellid_3`, `spelltrigger_3`, `spellcharges_3`, `spellppmRate_3`, `spellcooldown_3`, `spellcategory_3`, `spellcategorycooldown_3`, `spellid_4`, `spelltrigger_4`, `spellcharges_4`, `spellppmRate_4`, `spellcooldown_4`, `spellcategory_4`, `spellcategorycooldown_4`, `spellid_5`, `spelltrigger_5`, `spellcharges_5`, `spellppmRate_5`, `spellcooldown_5`, `spellcategory_5`, `spellcategorycooldown_5`, `bonding`, `description`, `PageText`, `LanguageID`, `PageMaterial`, `startquest`, `lockid`, `Material`, `sheath`, `RandomProperty`, `RandomSuffix`, `block`, `itemset`, `MaxDurability`, `area`, `Map`, `BagFamily`, `TotemCategory`, `socketColor_1`, `socketContent_1`, `socketColor_2`, `socketContent_2`, `socketColor_3`, `socketContent_3`, `socketBonus`, `GemProperties`, `RequiredDisenchantSkill`, `ArmorDamageModifier`, `duration`, `ItemLimitCategory`, `HolidayId`, `ScriptName`, `DisenchantID`, `FoodType`, `minMoneyLoot`, `maxMoneyLoot`, `flagsCustom`, `VerifiedBuild`) VALUES (36763, 13, 0, -1, 'Mythic Keystone: AhnKahet', 6710, 0, 0, 0, 1, 0, 0, 0, -1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1000, 0, 0, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, '', 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, '', 0, 0, 0, 0, 0, 1);
 
 UPDATE `item_template` SET `Quality` = 5, `Flags` = 32, `Bonding`= 1 WHERE `name` LIKE 'Mythic Keystone:%';
-
-ALTER TABLE `mythic_dungeon_times`
-	ADD COLUMN `keystone_entry` INT(10) NOT NULL AFTER `comment`;
-
-UPDATE `mythic_dungeon_times` SET `keystone_entry`='6208' WHERE  `mapId`=576;
-UPDATE `mythic_dungeon_times` SET `keystone_entry`='33456' WHERE  `mapId`=574;
-UPDATE `mythic_dungeon_times` SET `keystone_entry`='31645' WHERE  `mapId`=575;
-UPDATE `mythic_dungeon_times` SET `keystone_entry`='23956' WHERE  `mapId`=595;
-UPDATE `mythic_dungeon_times` SET `keystone_entry`='23674' WHERE  `mapId`=599;
-UPDATE `mythic_dungeon_times` SET `keystone_entry`='6209' WHERE  `mapId`=600;
-UPDATE `mythic_dungeon_times` SET `keystone_entry`='6210' WHERE  `mapId`=601;
-UPDATE `mythic_dungeon_times` SET `keystone_entry`='22486' WHERE  `mapId`=602;
-UPDATE `mythic_dungeon_times` SET `keystone_entry`='36763' WHERE  `mapId`=619;
-UPDATE `mythic_dungeon_times` SET `keystone_entry`='11100' WHERE  `mapId`=632;
-UPDATE `mythic_dungeon_times` SET `keystone_entry`='11085' WHERE  `mapId`=650;
-UPDATE `mythic_dungeon_times` SET `keystone_entry`='11115' WHERE  `mapId`=658;
-UPDATE `mythic_dungeon_times` SET `keystone_entry`='12143' WHERE  `mapId`=668;
-UPDATE `mythic_dungeon_times` SET `keystone_entry`='16308' WHERE  `mapId`=604;
-UPDATE `mythic_dungeon_times` SET `keystone_entry`='24010' WHERE  `mapId`=608;
